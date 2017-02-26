862f86b2d85babe66fb9c5111836484239adeb0d
Juozas Beniusis <beniusij@gmail.com>
2017-02-26 11:31:03 +0100

    Added nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users do
+    resource :profile
+  end
   resources :contacts, only: :create
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
 
-  root to: 'pages#home'
   get 'about', to: 'pages#about'
 end
